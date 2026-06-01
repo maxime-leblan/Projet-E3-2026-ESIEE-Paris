cef0b9dc7049935a0676aa2364fa32e2a00b0704
maxime-leblan <maxime.leblan421@gmail.com>
2026-06-01 15:49:32 +0200

    Ajout deuxième algorithme trilatération
    Implémentation de l'algorithme de trilatération utilisant les mesures des baromètres et seulement 3 capteurs UWB.

--- Code Source/Algorithme trilatération/Trilateration.h
+++ Code Source/Algorithme trilatération/Trilateration.h
@@ -9,17 +9,31 @@
 using namespace Eigen;
 
 /*
-Matrice contenant les coeffs du membre gauche des équations du système linéaire des équations des sphères de triangulations
+Matrice contenant les coeffs du membre gauche des équations du système linéaire des équations des sphères de triangulations avec 3 inconnues
 */
 extern Matrix<float, 6, 3> gA;
+/*
+Matrice contenant les coeffs du membre gauche des équations du système linéaire des équations des sphères de triangulations avec 2 inconnues
+*/
+extern Matrix<float, 3, 2> gA2D;
 
 /*
-Matrice contenant les coeffs du membre droit des équations du système linéaire des équations des sphères de triangulations
+Matrice contenant les coeffs du membre droit des équations du système linéaire des équations des sphères de triangulations avec 3 inconnues
 */
 extern Matrix<float, 6, 1> gB;
+/*
+Matrice contenant les coeffs du membre droit des équations du système linéaire des équations des sphères de triangulations avec 2 inconnues
+*/
+extern Matrix<float, 3, 1> gB2D;
 
 /*
-Calcule et renvoie les coordonnées d'un module en 3D en utilisant l'algorithme de trilatération
+Calcule et renvoie l'altitude en mètre par rapport au niveau de la mer à partir de la pression atmosphérique
+pAtmospheriqPression - pression atmosphérique en hPa
+*/
+float giveAltitude(float pAtmospheriqPression);
+
+/*
+Calcule et renvoie les coordonnées d'un module en 3D en utilisant l'algorithme de trilatération avec 4 ancres
 avec en paramètre la liste des ancres (pSensors) et la liste des distances entre chaque ancre avec le module
 dont on souhaite déterminer les coordonnées.
 pSensors - liste des modules jouants le rôle de capteurs
@@ -29,10 +43,27 @@ Return : les coordonnées du module dont on ne connaît pas la position
 V3 trilateration3D(UWBModuleList pSensors, unordered_map<int, float> pDistances);
 
 /*
+Calcule et renvoie les coordonnées d'un module en 3D en utilisant l'algorithme de trilatération avec seulement 3 ancres + l'altitude déjà connue
+avec en paramètre la liste des ancres (pSensors) et la liste des distances entre chaque ancre avec le module
+dont on souhaite déterminer les coordonnées.
+pSensors - liste des modules jouants le rôle de capteurs
+pDistances - liste des distances de chaque capteur avec le module de position indéterminée (tag)
+pAltitude - altitude du tag calculée préalablement avec la pression atmosphérique mesurée par les capteurs sur l'engin et le tag
+Return : les coordonnées du module dont on ne connaît pas la position
+*/
+V3 trilateration3D(UWBModuleList pSensors, unordered_map<int, float> pDistances, float pAltitude);
+
+/*
 Initialise les valeurs de la matrice A avec les positions des capteurs UWB
 pSensors - liste des modules jouants le rôle de capteurs
 */
 void initMatrixA(UWBModuleList pSensors);
+
+/*
+Initialise les valeurs de la matrice A2D avec les positions des capteurs UWB
+pSensors - liste des modules jouants le rôle de capteurs
+*/
+void initMatrixA2D(UWBModuleList pSensors);
 
 /*
 Initialise les valeurs de la matrice B avec les positions des capteurs UWB et les distances de chaque capteur par rapport au tag
@@ -40,3 +71,11 @@ pSensors - liste des modules jouants le rôle de capteurs
 pDistances - liste des distances de chaque capteur avec le module de position indéterminée
 */
 void initMatrixB(UWBModuleList pSensors, unordered_map<int, float> pDistances);
+
+/*
+Initialise les valeurs de la matrice B2D avec les positions des capteurs UWB et les distances de chaque capteur par rapport au tag
+pSensors - liste des modules jouants le rôle de capteurs
+pDistances - liste des distances de chaque capteur avec le module de position indéterminée
+pAltitude - coordonnée z du module dont la position est indéterminée
+*/
+void initMatrixB2D(UWBModuleList pSensors, unordered_map<int, float> pDistances, float pAltitude);
--- Code Source/Algorithme trilatération/main_test.cpp
+++ Code Source/Algorithme trilatération/main_test.cpp
@@ -39,5 +39,37 @@ int main()
 
     cout << "Véritable position tag1 : " << vTag1Position << "\n";
     cout << "Position tag1 calculée par trilatération : " << vRealTag1 << "\n";
-    
+
+    // ------------------------------------------------------------------------------------------------------------------------
+
+    // On veut maintenant calculer les coordonnées du Tag avec l'algorithme de trilatération utilisant 3 ancres et 2 baromètres
+    // on crée une nouvelle liste de capteurs
+    UWBModuleList vSensorsBis = UWBModuleList();
+    vector<V3> vSensorsPositionBis = {V3(0, 3, 0), V3(3, 6, 0), V3(6, 3, 0)};
+
+    // on les ajoute dans la liste des capteurs
+    for (int i = 1; i <= NUMBER_OF_SENSORS-1; i++)
+    {
+        UWBModule vTemp = UWBModule(i, vSensorsPositionBis[i - 1]);
+        vSensorsBis.addModule(vTemp.getId(), vTemp);
+    }
+
+    // on calcule les distances réelles entre chaque capteur
+    unordered_map<int, float> vDistancesBis;
+
+    for (int i = 1; i <= NUMBER_OF_SENSORS-1; i++)
+    {
+        vDistancesBis[i] = (vSensorsBis.getModule(i).getPosition() - vTag1.getPosition()).norm();
+    }
+
+    // on récupère les coordonnées du vTag1 avec notre algorithme de triangulation
+    initMatrixA2D(vSensorsBis);
+
+    float vBar1Mesure = 1013.25;
+    float vBar2Mesure = 1012.89;
+
+    float vAltitudeTag1 = abs(giveAltitude(vBar1Mesure) - giveAltitude(vBar2Mesure));
+
+    V3 vRealTag1Bis = trilateration3D(vSensorsBis, vDistancesBis, vAltitudeTag1);
+    cout << "Position tag1 calculée par trilatération avec baromètres : " << vRealTag1Bis << "\n";
 }
