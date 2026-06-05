cba10fa8e3db9f738b2b16a13ad8d3005f5673da
maxime-leblan <maxime.leblan421@gmail.com>
2026-06-05 14:36:45 +0200

    Modification documentation

--- Code Source/Algorithme_final_hub/librairie_hub/Polygone.hpp
+++ Code Source/Algorithme_final_hub/librairie_hub/Polygone.hpp
@@ -44,13 +44,13 @@ class Polygone
     const vector<V3>& getPoints() const;
 
     /*
-    Détermine si pPoint est à l'intérieur du Polygone
+    Détermine si pPoint, qui sera considéré comme projeté sur le plan du polygone, est à l'intérieur du Polygone
     pPoint - point dont on veut vérifier la position par rapport au polygone
     */
     bool isInside(V3 pPoint);
 
     /*
-    Donne la distance entre le point et le polygone
+    Donne la distance entre le point passé en paramètre, qui sera considéré comme projeté sur le plan du polygone, et le polygone
     pPoint - point dont on veut obtenir la distance par rapport au polygone
     */
     float getDistanceFrom(V3 pPoint);
