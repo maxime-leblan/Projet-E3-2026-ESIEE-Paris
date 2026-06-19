8ebd46a5090f9870098162417b31d508510d9a49
maxime-leblan <maxime.leblan421@gmail.com>
2026-06-19 18:55:41 +0200

    Modif documentation

--- Code Source/Algorithme_final_ancre/firmware-esp32-ancre/lib/InitialisationProtocol/InitialisationProtocol.hpp
+++ Code Source/Algorithme_final_ancre/firmware-esp32-ancre/lib/InitialisationProtocol/InitialisationProtocol.hpp
@@ -4,6 +4,6 @@
 #include "UWBModuleActions.hpp"
 
 /**
- * Fonction qui ordonne à l'ancre dont l'id est passé en paramètre d'attendre un message du Hub pour se transformer en tag, puis d'envoyer en boucle ses distances par rapport 
+ * Fonction qui ordonne à l'ancre dont l'id est passé en paramètre d'attendre un message du Hub pour se transformer en tag, puis d'envoyer en boucle ses distances par rapport à l'ancre statique (la seule qui reste une ancre sur les 4)
  */
 void runInitializationPhase(uint8_t pMyAnchorId);
