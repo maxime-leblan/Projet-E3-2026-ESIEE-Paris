f4bbdd8b9a09708f67d5e4acd6bb790e3da8f88f
maxime-leblan <maxime.leblan421@gmail.com>
2026-06-19 20:36:14 +0200

    Création main final hub

---
+++ Code Source/Algorithme_final_hub/firmware-esp32-wrover/lib/Mains/main_can.cpp
@@ -0,0 +1,16 @@
+#include "Config.hpp"
+#include "HubDataStorage.hpp"
+#include "WifiMessageManager.hpp"
+#include "GestionnaireAncres.hpp"
+#include "InitAnchorPosition.hpp"
+#include <ArduinoEigenDense.h>
+#include <ArduinoJson.h>
+
+#include "Trilateration.h"
+#include "GridLibrary.hpp"
+#include "Polygone.hpp"
+#include "RecuperationDonneesAncres.hpp"
+#include "ScreenCommunicationManager.hpp"
+#include "ListeDistanceLibrary.hpp"
+#include "LissagePlanExclusion.hpp"
+#include "CalibrationManager.hpp"
